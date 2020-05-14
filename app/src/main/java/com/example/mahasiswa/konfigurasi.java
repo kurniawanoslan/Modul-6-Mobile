82f0d5e2f31baaee892bae92a3b2c305cab208de
kurniawanoslan <64569651+kurniawanoslan@users.noreply.github.com>
2020-05-14 11:05:08 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/mahasiswa/konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.mahasiswa;
+
+public class konfigurasi {
+    public static final String URL_ADD="https://192.168.1.9/modul6/insert.php";
+    public static final String URL_GET_ALL="https://192.168.1.9/modul6/read.php";
+    public static final String URL_GET_MHS="https://192.168.1.9/modul6/select.php?id=";
+    public static final String URL_UPDATE_MHS="https://192.168.1.9/modul6/update.php";
+    public static final String URL_DELETE_MHS="https://192.168.1.9/modul6/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
